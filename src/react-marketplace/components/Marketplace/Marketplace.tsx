2d22b66719206c1995708adc61b9c5487ccfd977
Martin Fuster <mfuster@litebox.ai>
2022-02-18 18:32:19 -0300

    add marketplace load animation

--- src/react-marketplace/components/Marketplace/Marketplace.tsx
+++ src/react-marketplace/components/Marketplace/Marketplace.tsx
@@ -3,6 +3,8 @@ import Tile from '../Tile';
 import cn from 'classnames';
 import styles from './Marketplace.module.css';
 import { MarketplaceProps } from '../interfaces/marketplace';
+import { useTrail, animated } from 'react-spring';
+import Spinner from '../Spinner';
 
 const Marketplace: React.FC<MarketplaceProps> = ({
   integrations,
@@ -22,28 +24,49 @@ const Marketplace: React.FC<MarketplaceProps> = ({
   onUninstalled,
   uninstallText,
 }) => {
+  const isReady = integrations.length > 0;
+  const trail = useTrail(integrations.length, {
+    opacity: 1,
+    y: 0,
+    config: { mass: 2, tension: 300, friction: 40 },
+    from: {
+      opacity: 0,
+      y: 50,
+    },
+  });
+
   return (
     <div className={cn(styles.wrapper, className)}>
-      {integrations?.map((integration) => (
-        <Tile
-          key={integration.integrationId}
-          classes={classes}
-          hideImages={hideImages}
-          hideLink={hideLink}
-          images={images}
-          installText={installText}
-          linkText={linkText}
-          onInstalled={onInstalled}
-          onMainActionClick={onMainActionClick}
-          onUninstalled={onUninstalled}
-          uninstallText={uninstallText}
-          getCustomBody={getCustomBody}
-          getInstallUrl={getInstallUrl}
-          onAuthentication={onAuthentication}
-          onUninstall={onUninstall}
-          {...integration}
-        />
-      ))}
+      {isReady ? (
+        trail.map((props, i) => (
+          <animated.div style={props} key={i}>
+            {
+              <Tile
+                key={integrations[i].integrationId}
+                classes={classes}
+                hideImages={hideImages}
+                hideLink={hideLink}
+                images={images}
+                installText={installText}
+                linkText={linkText}
+                onInstalled={onInstalled}
+                onMainActionClick={onMainActionClick}
+                onUninstalled={onUninstalled}
+                uninstallText={uninstallText}
+                getCustomBody={getCustomBody}
+                getInstallUrl={getInstallUrl}
+                onAuthentication={onAuthentication}
+                onUninstall={onUninstall}
+                {...integrations[i]}
+              />
+            }
+          </animated.div>
+        ))
+      ) : (
+        <div className={cn(styles['loading-wrapper'])}>
+          <Spinner className={classes?.spinner} />
+        </div>
+      )}
     </div>
   );
 };
--- src/react-marketplace/components/Tile/Tile.tsx
+++ src/react-marketplace/components/Tile/Tile.tsx
@@ -5,6 +5,7 @@ import styles from './Tile.module.css';
 import Link from '../Link';
 import Title from '../Title';
 import Card from '../Card';
+import Spinner from '../Spinner';
 import cn from 'classnames';
 import { TileProps } from '../interfaces/marketplace';
 import useTile from '../hooks/useTile';
@@ -72,21 +73,7 @@ const Tile: React.FC<TileProps> = ({
               </Link>
             )}
             {loading ? (
-              <svg
-                className={cn(styles['spinner'], classes?.spinner)}
-                width="62"
-                height="62"
-                viewBox="0 0 62 62"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M58.9999 31C58.9999 24.3836 56.6569 17.981 52.3863 12.9274C48.1157 7.87376 42.1935 4.49572 35.6698 3.39223C29.1461 2.28874 22.4424 3.53109 16.7473 6.89899C11.0522 10.2669 6.73378 15.5428 4.5577 21.7911C2.38163 28.0394 2.48851 34.8564 4.85942 41.0334C7.23032 47.2104 11.712 52.3483 17.5099 55.536C23.3078 58.7237 30.0472 59.7553 36.5331 58.4478"
-                  stroke="#F83420"
-                  strokeWidth="6"
-                  strokeLinecap="round"
-                />
-              </svg>
+              <Spinner className={classes?.spinner} />
             ) : (
               <Button onClick={handleClick} isInstalled={isInstalled} className={classes?.button}>
                 {buttonText}
