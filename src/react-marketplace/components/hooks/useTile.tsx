01ab8dd208b9ce11943fb34c83830b4f706f171b
Martin Fuster <mfuster@litebox.ai>
2022-05-10 14:57:43 -0300

    adapt bundle for js

--- src/react-marketplace/components/hooks/useTile.tsx
+++ src/react-marketplace/components/hooks/useTile.tsx
@@ -77,7 +77,7 @@ const useTile = ({
         if (onInstallClick) {
           onInstallClick?.(url);
         } else {
-          window.open(url, '_parent');
+          window.open(url, '_blank');
         }
       }
     }
--- src/react-marketplace/rollup.config.js
+++ src/react-marketplace/rollup.config.js
@@ -3,16 +3,19 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
 
 const packageJson = require('./package.json');
 
 export default [
   {
+    external: ['react'],
     input: 'index.ts',
     output: [
       {
         file: packageJson.main,
-        format: 'cjs',
+        format: 'iife',
+        name: 'Fusebit',
         sourcemap: true,
       },
       {
@@ -21,7 +24,17 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: '../../tsconfig.json' }), postcss({ modules: true })],
+    plugins: [
+      resolve({
+        browser: true,
+      }),
+      commonjs(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      typescript({ tsconfig: '../../tsconfig.json' }),
+      postcss({ modules: true }),
+    ],
   },
   {
     input: 'dist/esm/types/index.d.ts',
